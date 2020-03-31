74409866b0083a7addb5b88866370070a3d2e31f
Sandeep Singh Rawat <40137450+kilzol@users.noreply.github.com>
2020-03-31 17:57:06 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
 #integrating scraper and crawler and making tool zoldyck
 import os
 
-#os.system("tput setaf 1")
-#os.system("color 1b")
+os.system("tput setaf 1")
+os.system("color 1b")
 print("\t\t\t\t\t\t\tZOLDYCK")
-#os.system("tput setaf 5")
+os.system("tput setaf 5")
 print("\t\t\t\t\t\t\t_______")
 while True:
 	print("""\t\t\tpress 1: To crawl a website
@@ -20,12 +20,12 @@ while True:
 		file = open('website_link.txt','w')
 		file.write(website)
 		file.close()
-		os.system("py crawl.py")
-		#os.system("python3 crawl.py")
+		#os.system("py crawl.py")
+		os.system("python3 crawl.py")
 		
 
 	elif int(x) == 2:
-		os.system("py scrap.py")
-		#os.system("python3 scrap.py")
+		#os.system("py scrap.py")
+		os.system("python3 scrap.py")
 	elif int(x) == 4:
 		exit()
